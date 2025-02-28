f271ceab1cd716e54c1d6625667df9e22a30e228
anwar juniansyah <anwarjuniansyah136@gmail.com>
2025-02-28 21:32:03 +0700

    seven

---
+++ src/main/java/com/perpustakaan/beperpustakaan/config/CorsConfig.java
@@ -0,0 +1,21 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Mengizinkan semua endpoint
+                        .allowedOrigins("*") // Mengizinkan akses dari semua domain
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Metode HTTP yang diizinkan
+                        .allowedHeaders("*") // Mengizinkan semua header
+                        .allowCredentials(true); // Mengizinkan penggunaan kredensial (opsional)
+            }
+        };
+    }
+}
